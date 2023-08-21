c854967aea5b32184015382bed6408fe9f886251
Mike Murray <mike.murray@peachpayments.com>
2023-08-21 15:44:49 +0200

    refactor: 🚨 Linting fixes

--- src/components/link.svelte
+++ src/components/link.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
   export let href: string
-  export let newTab: boolean = false
-  export let addClass: string = ''
+  export let newTab = false
+  export let addClass = ''
   const newClass = `${addClass} text-secondary-400 underline hover:text-secondary-300 py-2`
 </script>
 
 {#if newTab}
-  <a href={href} class={newClass} target="_blank">
+  <a {href} class={newClass} target="_blank">
     <slot />
   </a>
 {:else}
-  <a href={href} class={newClass}>
+  <a {href} class={newClass}>
     <slot />
   </a>
 {/if}
