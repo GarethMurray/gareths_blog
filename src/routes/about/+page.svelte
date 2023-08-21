7fc7584fa1eb622160a3f4a72032a0fc1c7f8a26
Mike Murray <mike.murray@peachpayments.com>
2023-08-21 15:47:10 +0200

    refactor: ♻️ Update the about page to folder based routing

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,18 @@
+<div class="prose prose-invert">
+
+  <h2> Some stuff about me / thoughts I have </h2>
+  
+  <li>I have an unhealthy relationship with optimisation - you know when you optimise for an event that'll never happen? 🚤</li>
+  <li>Definitely a dog person 🐕</li>
+  <li>True mechanical keyboard endgame is split low profile without row stagger</li>
+    <li>Colemak-dh ⌨️ over Workman over Dvorak</li>
+  <li>I'm an advocate for simplicity as far as possible 👍</li>
+  <li>I am currently a Fullstack Tech Lead at Peach Payments 🥞</li>
+  <li>I also run the Remix-JHB meetup and React.Africa 🚀</li>
+  <li>Framework wars are a distraction. </li>
+  <li>But also, I really like Solidjs</li>
+  <li>This was built with Sveltekit, and was updated after the change from file-based to folder-based routing.</li>
+  <li>SFCs are great, they remind me of Vue, wich I really enjoyed</li>
+  <li>I really want to work with PandaCSS</li>
+
+</div>
