e9d1bd7e74b5160683a65ec78330f7d1091d18e3
Gareth Murray <64191834+GarethMurray@users.noreply.github.com>
2023-10-22 14:33:31 +0200

    feat: :lipstick: Updated theme again

--- src/components/underline.svelte
+++ src/components/underline.svelte
@@ -1,5 +1,5 @@
 <div class="relative mt-2 h-px bg-dark-50 overflow-hidden">
   <div
-    class="absolute h-px w-full bg-gradient-to-r from-primary-600 via-light-900 to-tertiary-900 transition-all duration-500 opacity-0 group-hover:opacity-90 -translate-x-full group-hover:translate-x-0 "
+    class="absolute h-px w-full bg-gradient-to-r from-tertiary-400 via-tertiary-600 to-tertiary-900 transition-all duration-500 opacity-0 group-hover:opacity-90 -translate-x-full group-hover:translate-x-0 "
   />
 </div>
